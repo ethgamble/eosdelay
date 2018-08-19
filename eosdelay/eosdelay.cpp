4ce0c4bcce4b4cfdf91cf168f36cd91a29204400
dabdevelop <develop@dab.vc>
2018-08-19 16:43:21 +0800

    add memo to notify

--- eosdelay/eosdelay.cpp
+++ eosdelay/eosdelay.cpp
@@ -1,5 +1,9 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/types.hpp>
+#include <eosiolib/action.hpp>
+#include <eosiolib/symbol.hpp>
+#include <eosiolib/time.hpp>
 #include <eosiolib/transaction.hpp>
 
 using namespace eosio;
@@ -31,7 +35,7 @@ public:
             out.actions.emplace_back(
                 permission_level{_self, N(active)},
                 _self, N(delay),
-                make_tuple(due, from, to, quant, string("delay"))); //将指定行为绑定到该交易上
+                make_tuple(due, from, to, quant, string("delay ") + int2str(due) + string(" now ") + int2str(now()) + string(" remain ") + int2str(due - now()))); //将指定行为绑定到该交易上
             //设置延迟时间，单位为1秒
             if((due - now()) / 2 <= 1){
                 out.delay_sec = due - now() - 1;
@@ -39,19 +43,26 @@ public:
                 out.delay_sec = (due - now()) / 2;
             }
             out.send(_next_id(), _self, true); //发送交易，第一个参数为该次交易发送id，每次需不同。如果两个发送id相同，则视第三个参数replace_existing来定是覆盖还是直接失败。
-        } else if(memo == "delay"){
-            if(current_time() >= due * 1000000ll || current_time() > (due * 1000000ll - 500000ll)){
+        } else if(startWith(memo, "delay")){
+            // if(current_time() >= due * 1000000ll || current_time() > (due * 1000000ll - 500000ll)){
+            if(current_time() >= due * 1000000ll){
                 action(
                     permission_level{from, N(active)},
                     N(eosio.token), N(transfer),
-                    make_tuple(from, to, quant, ""))
+                    make_tuple(from, to, quant, string("")))
+                .send();
+                // To notifiy the transfer has been sent
+                action(
+                    permission_level{from, N(active)},
+                    N(eosio.token), N(transfer),
+                    make_tuple(from, _self, quant, string("delay ") + int2str(due) + string(" excute ") + int2str(now())))
                 .send();
             } else {
                 transaction out; //构造交易
                 out.actions.emplace_back(
                     permission_level{_self, N(active)},
                     _self, N(delay),
-                    make_tuple(due, from, to, quant, string("delay")));
+                    make_tuple(due, from, to, quant, string("delay ") + int2str(due) + string(" now ") + int2str(now()) + string(" remain ") + int2str(due - now())));
                 out.delay_sec = 1;
                 out.send(_next_id(), _self, true); 
             }
@@ -69,6 +80,24 @@ public:
     }
 
 private:
+    string int2str(uint64_t x){
+        string tmp(""), ans("");
+        while(x > 0)
+        {
+            tmp += (x % 10) + 48;
+            x /= 10;
+        }
+        for(int i = tmp.size() - 1; i >= 0; i--)
+        {
+            ans += tmp[i];
+        }
+        return ans;
+    }  
+
+    bool startWith(const string &str, const string &head) {
+	    return str.compare(0, head.size(), head) == 0;
+    }
+
     // @abi table global i64
     struct global{
         account_name owner;
@@ -80,10 +109,12 @@ private:
     typedef eosio::multi_index<N(global), global> global_index;
     global_index _global;
 
-    //TODO  add more the target contract struct here
+    // TODO add other contract's struct to inspect data
+
 
 
 };
+
  #define EOSIO_ABI_EX( TYPE, MEMBERS ) \
  extern "C" { \
     void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
